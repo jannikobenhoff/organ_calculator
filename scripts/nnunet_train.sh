f8fe7f0a0a5948d252fb78db5a092fb0f467ebbb
jannikobenhoff <hello@jannikobenhoff.com>
2024-10-30 13:15:55 -0400

    update script

--- scripts/nnunet_train.sh
+++ scripts/nnunet_train.sh
@@ -3,11 +3,12 @@
 SBATCH --job-name=<jannik_nnunet> # give your job a name
 #SBATCH --nodes=1
 SBATCH --cpus-per-task=15
-SBATCH --time=03:00:00 # set this time according to your need
+SBATCH --time=02:00:00 # set this time according to your need
 SBATCH --mem=200G # how much RAM will your notebook consume? 
 SBATCH --gres=gpu:1 # if you need to use a GPU
-SBATCH -p sablab-gpu # specify partition
+# SBATCH -p sablab-gpu # specify partition
 SBATCH --nodelist ai-gpu01
+
 module purge
 module load anaconda3
 source ../venv/bin/activate
