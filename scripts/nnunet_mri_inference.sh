c5649fbe60e0e139ac6f871b62c6896d8eb6a0fd
jannikobenhoff <hello@jannikobenhoff.com>
2024-12-08 12:35:12 -0500

    udpate script

--- scripts/nnunet_mri_inference.sh
+++ scripts/nnunet_mri_inference.sh
@@ -19,5 +19,22 @@ export nnUNet_raw="/home/jao4016/organ_calculator/data/nnUNet_raw"
 export nnUNet_preprocessed="/home/jao4016/organ_calculator/data/nnUNet_preprocessed"
 export nnUNet_results="/home/jao4016/organ_calculator/data/nnUNet_results"
 
-nnUNetv2_predict -i ../data/MRI/input/case1 -o ../data/MRI/output/case1 -d 101 -c 2d
+nnUNetv2_predict -i ../data/MRI/input/case2 -o ../data/MRI/output/case2 -d 101 -c 2d
+
+nnUNetv2_predict -i ../data/MRI/input/case3 -o ../data/MRI/output/case3 -d 101 -c 2d
+
+nnUNetv2_predict -i ../data/MRI/input/case4 -o ../data/MRI/output/case4 -d 101 -c 2d
+
+nnUNetv2_predict -i ../data/MRI/input/case5 -o ../data/MRI/output/case5 -d 101 -c 2d
+
+nnUNetv2_predict -i ../data/MRI/input/case6 -o ../data/MRI/output/case6 -d 101 -c 2d
+
+nnUNetv2_predict -i ../data/MRI/input/case7 -o ../data/MRI/output/case7 -d 101 -c 2d
+
+nnUNetv2_predict -i ../data/MRI/input/case8 -o ../data/MRI/output/case8 -d 101 -c 2d
+
+nnUNetv2_predict -i ../data/MRI/input/case9 -o ../data/MRI/output/case9 -d 101 -c 2d
+
+nnUNetv2_predict -i ../data/MRI/input/case10 -o ../data/MRI/output/case10 -d 101 -c 2d
+
 # FileNotFoundError: [Errno 2] No such file or directory: '/home/jao4016/organ_calculator/data/nnUNet_results/Dataset101_Totalsegmentator/nnUNetTrainer__nnUNetPlans__2d/fold_2/checkpoint_final.pth'
