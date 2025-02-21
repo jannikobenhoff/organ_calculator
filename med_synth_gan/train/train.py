78fd487e8a3cbf3cebe5a7437ec4e69fb086107a
jannikobenhoff <hello@jannikobenhoff.com>
2025-02-21 12:34:02 -0500

    update

--- med_synth_gan/train/train.py
+++ med_synth_gan/train/train.py
@@ -184,7 +184,7 @@ def parse_args(argv):
     parser.add_argument(
         "-lr",
         "--learning-rate",
-        default=5e-5, #5e-5
+        default=1e-5, #5e-5
         type=float,
         help="Learning rate (default: %(default)s)",
     )
@@ -198,7 +198,7 @@ def parse_args(argv):
     parser.add_argument(
         "-bce",
         "--bce",
-        default=True,
+        default=False,
         type=bool,
         help="Use BCE Loss (default: %(default)s)",
     )
