b244e9e71b8065548a903739b838f125439a3a0a
jannikobenhoff <hello@jannikobenhoff.com>
2025-02-21 15:58:46 -0500

    update

--- med_synth_gan/train/train.py
+++ med_synth_gan/train/train.py
@@ -124,10 +124,10 @@ class MedSynthGANModule(pl.LightningModule):
         # 3) Return them in the correct Lightning format
         return (
             [opt_g, opt_d],
-            [
-                {"scheduler": scheduler_g, "interval": "epoch", "frequency": 1, "name": "lr_g"},
-                {"scheduler": scheduler_d, "interval": "epoch", "frequency": 1, "name": "lr_d"},
-            ]
+            # [
+            #     {"scheduler": scheduler_g, "interval": "epoch", "frequency": 1, "name": "lr_g"},
+            #     {"scheduler": scheduler_d, "interval": "epoch", "frequency": 1, "name": "lr_d"},
+            # ]
         )
 
 class CustomProgressBar(TQDMProgressBar):
@@ -184,14 +184,14 @@ def parse_args(argv):
     parser.add_argument(
         "-lr",
         "--learning-rate",
-        default=1e-5, #5e-5
+        default=5e-5, #5e-5
         type=float,
         help="Learning rate (default: %(default)s)",
     )
     parser.add_argument(
         "-lr_d",
         "--learning-rate-discriminator",
-        default=5e-5, # should be larger than Generator for MSE 1e-4
+        default=2e-4, # should be larger than Generator for MSE 1e-4
         type=float,
         help="Learning rate (default: %(default)s)",
     )
