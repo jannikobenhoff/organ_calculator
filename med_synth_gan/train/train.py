c14278c70499cb37c5d429e208cf26fa65951ebf
jannikobenhoff <hello@jannikobenhoff.com>
2025-02-20 23:53:59 -0500

    update

--- med_synth_gan/train/train.py
+++ med_synth_gan/train/train.py
@@ -238,7 +238,7 @@ def main(argv):
     # Inference
     inference_callback = VolumeInferenceCallback(
         test_volume_path="/midtier/sablab/scratch/data/jannik_data/synth_data/Dataset5008_AMOS_CT_2022/imagesTs/AMOS_CT_2022_000001_0000.nii.gz",
-        output_dir=f"inference_mse", #_{args.batch_size}_{args.learning_rate}_{args.learning_rate_discriminator}",
+        output_dir=f"inference_bce_same1", #_{args.batch_size}_{args.learning_rate}_{args.learning_rate_discriminator}",
     )
 
     trainer = pl.Trainer(
