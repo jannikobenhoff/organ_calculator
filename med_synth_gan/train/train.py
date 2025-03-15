af6add0e6122db97e4f7a16a13662b7fa8e7e248
jannikobenhoff <hello@jannikobenhoff.com>
2025-03-15 10:02:51 -0400

    update

--- med_synth_gan/train/train.py
+++ med_synth_gan/train/train.py
@@ -218,14 +218,14 @@ def parse_args(argv):
     parser.add_argument(
         "-lr",
         "--learning-rate",
-        default=4e-6, #5e-5
+        default=4e-5, #5e-5
         type=float,
         help="Learning rate (default: %(default)s)",
     )
     parser.add_argument(
         "-lr_d",
         "--learning-rate-discriminator",
-        default=5e-6, # should be larger than Generator for MSE 1e-4
+        default=5e-5, # should be larger than Generator for MSE 1e-4
         type=float,
         help="Learning rate (default: %(default)s)",
     )
