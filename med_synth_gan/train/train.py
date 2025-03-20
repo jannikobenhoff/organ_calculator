58201ba64f0e54b63db4fd61c3b6a6c2eb3aaddc
jannikobenhoff <hello@jannikobenhoff.com>
2025-03-20 12:33:00 -0400

    update

--- med_synth_gan/train/train.py
+++ med_synth_gan/train/train.py
@@ -204,7 +204,7 @@ def parse_args(argv):
     parser.add_argument(
         "-e",
         "--epochs",
-        default=20,
+        default=50,
         type=int,
         help="Number of epochs (default: %(default)s)",
     )
@@ -218,14 +218,14 @@ def parse_args(argv):
     parser.add_argument(
         "-lr",
         "--learning-rate",
-        default=5e-6, #5e-5
+        default=2e-5, #5e-5
         type=float,
         help="Learning rate (default: %(default)s)",
     )
     parser.add_argument(
         "-lr_d",
         "--learning-rate-discriminator",
-        default=1e-5, # should be larger than Generator for MSE 1e-4
+        default=1e-4, # should be larger than Generator for MSE 1e-4
         type=float,
         help="Learning rate (default: %(default)s)",
     )
