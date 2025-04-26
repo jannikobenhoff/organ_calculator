8c7a9bdfb1c0749049fee1955e0ad5f64ed1fc11
jannikobenhoff <hello@jannikobenhoff.com>
2025-04-25 21:45:49 -0400

    update

--- med_synth_gan/train/train.py
+++ med_synth_gan/train/train.py
@@ -120,7 +120,7 @@ class MedSynthGANModule(nn.Module):
             real_mri = self.aug(real_mri)
 
             pred_real = self.D_mri(real_mri)
-            loss_real = self.criterion_GAN(pred_real, torch.ones_like(pred_real) * 0.9)  # Label smoothing
+            loss_real = self.criterion_GAN(pred_real, torch.ones_like(pred_real) * 1)  # Label smoothing
             pred_fake = self.D_mri(fake_mri)
             loss_fake = self.criterion_GAN(pred_fake, torch.zeros_like(pred_fake))
             loss_D = (loss_real + loss_fake) * 0.5
@@ -258,14 +258,14 @@ def parse_args(argv):
     parser.add_argument(
         "-lr",
         "--learning-rate",
-        default=2e-5 , #5e-5
+        default=1e-5 , #5e-5
         type=float,
         help="Learning rate (default: %(default)s)",
     )
     parser.add_argument(
         "-lr_d",
         "--learning-rate-discriminator",
-        default= 5e-5,  # should be larger than Generator for MSE 1e-4
+        default= 2e-5,  # should be larger than Generator for MSE 1e-4
         type=float,
         help="Learning rate (default: %(default)s)",
     )
