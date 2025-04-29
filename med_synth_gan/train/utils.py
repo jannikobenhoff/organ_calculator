faa64df263e9902bd10762c65ed88254f552c5ad
jannikobenhoff <hello@jannikobenhoff.com>
2025-04-28 20:57:38 -0400

    update lr

--- med_synth_gan/train/utils.py
+++ med_synth_gan/train/utils.py
@@ -12,9 +12,10 @@ def save_debug_images(real_ct, real_mri, step, dim):
                           f"ct_train_slice{step}.png",
                           normalize=True)
     else:                                     # 3-D → pick middle slice
-        mid = real_ct.shape[2] // 2           # D // 2
-        ct_mid  = real_ct[0, :, mid]          # 1xHxW
-        mri_mid = real_mri[0, :, mid]
+        mid = real_ct.shape[2] // 2  # D // 2
+        ct_mid = real_ct[0, :, mid, :, :]  # 1 × H × W
+        mri_mid = real_mri[0, :, mid, :, :]  # 1 × H × W
+
         vutils.save_image(mri_mid,
                           f"mri_train_slice{step}.png",
                           normalize=True)
