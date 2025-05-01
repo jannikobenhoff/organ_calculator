7e1673cccfde5e1e47b5a1c328428a06d1ef1253
jannikobenhoff <hello@jannikobenhoff.com>
2025-05-01 13:41:25 -0400

    update lr

---
+++ med_synth_gan/scripts/util.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --partition=sablab-gpu   # cluster-specific
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=jannik_med_synth_gan
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=15
+#SBATCH --time=05:00:00   # HH/MM/SS
+#SBATCH --mem=200G   # memory requested, units available: K,M,G,T
+#SBATCH --output /home/jao4016/log/job-%j.out
+#SBATCH --error /home/jao4016/log/job-%j.err
+
+module purge
+module load anaconda3
+source ../../venv/bin/activate
+
+cd ../train || exit
+
+python -u utils.py
