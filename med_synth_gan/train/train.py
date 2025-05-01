cb470daa48045c1742b2c12f39cc9ce0ea8cfa08
jannikobenhoff <hello@jannikobenhoff.com>
2025-05-01 17:45:56 -0400

    fix

--- med_synth_gan/train/train.py
+++ med_synth_gan/train/train.py
@@ -278,7 +278,7 @@ def parse_args(argv):
     parser.add_argument(
         "-lambda_grad",
         "--lambda-grad",
-        default=0,#1e-4,#1e-7,
+        default=1e-6,#1e-4,#1e-7,
         type=float,
         help="Weight for total-variation (default: %(default)s)",
     )
@@ -292,7 +292,7 @@ def parse_args(argv):
     parser.add_argument(
         "-lr_d",
         "--learning-rate-discriminator",
-        default=2e-4,  # should be larger than Generator for MSE 1e-5
+        default=1e-4,  # should be larger than Generator for MSE 1e-5
         type=float,
         help="Learning rate (default: %(default)s)",
     )
